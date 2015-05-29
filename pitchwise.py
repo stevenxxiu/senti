2a01479cafbfeca20578423b78d5d232616378f4
Steven Xu <stevenxxiu@gmail.com>
2015-05-30 03:45:02 +1000

    added pitchwise sentence prediction but we can't just sum the word vectors, word2vec is not so simple, put it in the training set instead

---
+++ pitchwise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import itertools
+import json
+
+import lxml.etree
+from nltk.tokenize import sent_tokenize
+
+
+def parse(path):
+    res = []
+    with open(path, 'r', encoding='utf-8') as sr:
+        root = lxml.etree.fromstringlist(itertools.chain('<root>', sr, '</root>'))
+        obj = {}
+        for node in root:
+            tag = node.tag.lower()
+            if tag == 'docno':
+                obj['docno'] = int(node.text.strip())
+            elif tag == 'body':
+                obj['text'] = node.text.strip()
+                res.append(obj)
+                obj = {}
+    return res
+
+
+def to_json(objs, path):
+    with open(path, 'w') as sr:
+        for obj in objs:
+            for sent in sent_tokenize(obj['text']):
+                json.dump({sent: None}, sr)
+                sr.write('\n')
+
+
+def main():
+    to_json(parse('data/pitchwise/test.xml'), 'data/pitchwise/test.json')
+
+
+if __name__ == '__main__':
+    main()
--- predict.py
+++ predict.py
@@ -4,13 +4,15 @@ import itertools
 import json
 import os
 import re
+import numpy as np
 
-data_dir = 'data/twitter'
+data_dir = 'data/pitchwise'
 data_files = [os.path.join(data_dir, path) for path in ('train.json', 'dev.json')]
 liblinear_files = [os.path.join(data_dir, path) for path in ('train.txt', 'dev.txt')]
 
 
 def normalize_text(text):
+    # XXX use word_tokenize?
     text = re.sub(r'([\.",()!?;:])', r' \1 ', text.lower())
     text = re.sub(r'\s+', ' ', text)
     return text
@@ -23,10 +25,10 @@ def word2vec():
             text = normalize_text(next(iter(json.loads(line).keys())))
             out_sr.write('_*{} {}\n'.format(i, text))
     os.system(r'''
-        time ./word2vec/word2vec -train {0}/alldata.txt -output {0}/vectors.txt -cbow 0 -size 100 -window 10
+        time word2vec/word2vec -train {0}/alldata.txt -output {0}/vectors.txt -cbow 0 -size 100 -window 10
         -negative 5 -hs 0 -sample 1e-4 -threads 40 -binary 0 -iter 20 -min-count 1 -sentence-vectors 1
     '''.replace('\n', '').format(data_dir))
-    with open('{}/vectors.txt'.format(data_dir), encoding='ISO-8859-1') as in_sr,\
+    with open('{}/vectors.txt'.format(data_dir), encoding='ISO-8859-1') as in_sr, \
             open('{}/sentence_vectors.txt'.format(data_dir), 'w') as out_sr:
         for line in in_sr:
             if line.startswith('_*'):
@@ -42,21 +44,43 @@ def prepare_liblinear():
                     classif = next(iter(json.loads(data_line).values()))
                     if classif is None:
                         continue
-                    classif = classif//5 - 1
+                    classif //= 5
                     out_data = ' '.join('{}:{}'.format(i + 1, v) for i, v in enumerate(vect_line.strip().split()[1:]))
                     out_sr.write('{} {}\n'.format(classif, out_data))
 
 
-def train_predict():
-    os.system('liblinear/train -s 0 {0}/train.txt {0}/model.logreg'.format(data_dir))
-    os.system('liblinear/predict -b 1 {0}/dev.txt {0}/model.logreg {0}/out.logreg'.format(data_dir))
+def train_dev():
+    os.system('liblinear/train -s 0 {0}/train.txt {0}/train.logreg'.format(data_dir))
+    os.system('liblinear/predict -b 1 {0}/dev.txt {0}/train.logreg {0}/dev.logreg'.format(data_dir))
+
+
+def test():
+    if not os.path.exists('{0}/test.json'.format(data_dir)):
+        return
+    # load word2vec vectors
+    vects = {}
+    with open('{}/vectors.txt'.format(data_dir), encoding='ISO-8859-1') as in_sr:
+        for line in in_sr:
+            if not line.startswith('_*'):
+                tokens = line.strip().split()
+                vects[tokens[0]] = np.array(list(map(float, tokens[1:])))
+    # convert test data to vectors, skipping unknown words
+    with open('{0}/test.json'.format(data_dir), 'r') as in_sr, open('{0}/test.txt'.format(data_dir), 'w') as out_sr:
+        for line in in_sr:
+            sentence = normalize_text(next(iter(json.loads(line).keys())))
+            sentence = normalize_text("Gas by my house hit $3.39!!!! I'm going to Chapel Hill on Sat. :)")
+            sentence_vect = np.zeros(len(next(iter(vects.values()))))
+            for token in sentence.split():
+                if token in vects:
+                    sentence_vect += vects[token]
+            out_sr.write('-1 {}\n'.format(' '.join('{}:{:.6f}'.format(i + 1, v) for i, v in enumerate(sentence_vect))))
 
 
 def main():
-    word2vec()
-    prepare_liblinear()
-    train_predict()
-
+    # word2vec()
+    # prepare_liblinear()
+    # train_dev()
+    test()
 
 if __name__ == '__main__':
     main()
