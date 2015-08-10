fd6d099e31e6d91181e13150e5fbf23fd54b96fa
Steven Xu <stevenxxiu@gmail.com>
2015-08-11 06:55:31 +1000

    transforms are either implemented in sklearn already or non-vector based and hard to port to a transform; moved normalization stuff to preprocess.py

---
+++ senti/preprocess.py
@@ -0,0 +1,13 @@
+
+import re
+
+__all__ = ['normalize_urls', 'tokenize']
+
+
+def normalize_urls(text):
+    return re.sub(r'\S{,4}://\S+', '_URL', text)
+
+def tokenize(text):
+    text = re.sub(r'\S{,4}://\S+', '_URL', text)
+    text = re.sub(r'([\.\",()!?;:])', r' \1 ', text)
+    return text.strip().split()
--- senti/transforms/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from senti.transforms.binarize import *
-from senti.transforms.lower import *
-from senti.transforms.normalize import *
-from senti.transforms.proptokens import *
-from senti.transforms.proptotal import *
-from senti.transforms.vecconcat import *
