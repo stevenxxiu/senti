697cdb9f8a735837e7ea4d8d1e50c8d2e7b84873
Steven Xu <stevenxxiu@gmail.com>
2015-09-04 10:23:18 +1000

    implemented negations

---
+++ senti/features/negations.py
@@ -0,0 +1,41 @@
+
+import re
+
+from sklearn.base import BaseEstimator
+
+__all__ = ['Negations']
+
+# Note that Potts didn't included the comma in the definition, but his examples assume it terminates a context.
+TERMINATORS = frozenset('.:;!?,')
+NEGATION_RE = re.compile(r'''(?:
+    ^(?:never|no|nothing|nowhere|noone|none|not|havent|hasnt|hadnt|cant|couldnt|shouldnt|wont|wouldnt|dont|doesnt|didnt|isnt|arent|aint)$
+)|n't''', re.X | re.I | re.U)
+
+
+class Negations(BaseEstimator):
+    '''
+    Appends '_NEG' to tokens in negated contexts.
+    '''
+
+    @staticmethod
+    def is_negation(token):
+        return bool(NEGATION_RE.search(token))
+
+    @staticmethod
+    def is_terminator(token):
+        return token and TERMINATORS.issuperset(token)
+
+    def fit(self, docs, y=None):
+        return self
+
+    def transform(self, docs):
+        for doc in docs:
+            negated = False
+            for i, token in enumerate(doc):
+                if negated and self.is_terminator(token):
+                    negated = False
+                if negated:
+                    doc[i] = 'NEG_' + token
+                if not negated and self.is_negation(token):
+                    negated = True
+            yield doc
