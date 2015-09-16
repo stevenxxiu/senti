df48d3bac84b80338efb417c01706eeef1712ed6
Steven Xu <stevenxxiu@gmail.com>
2015-09-16 13:23:23 +1000

    include labels in the twitter test data so we can test (but only optimize on train & dev) locally

--- senti/data/twitter.py
+++ senti/data/twitter.py
@@ -12,10 +12,15 @@ def main():
             for i, line in enumerate(in_sr):
                 text, label = next(iter(json.loads(line).items()))
                 out_sr.write(json.dumps({'id': '{}_{}'.format(name, i), 'text': text, 'label': int(label)//5}) + '\n')
-    with open('input/SemEval2015-task10-test-B-input.txt') as in_sr, open('test.json', 'w') as out_sr:
-        for line in in_sr:
-            doc_id, text = re.match(r'^NA\t(T[0-9]+)\tunknwn\t(.+)', line).groups()
-            out_sr.write(json.dumps({'id': doc_id, 'text': text}) + '\n')
+    with open('input/SemEval2015-task10-test-B-input.txt') as in_sr, open('input/SemEval2015-task10-test-B-gold.txt') \
+            as labels_sr, open('test.json', 'w') as out_sr:
+        for line, label_line in zip(in_sr, labels_sr):
+            doc_id, text = re.match(r'^NA\t(T\d+)\tunknwn\t(.+)', line).groups()
+            doc_id_label, label = re.match(r'\d+\t(T\d+)\t(negative|neutral|positive)', label_line).groups()
+            assert doc_id == doc_id_label
+            out_sr.write(json.dumps({
+                'id': doc_id, 'text': text, 'label': {'negative': 0, 'neutral': 1, 'positive': 2}[label]
+            }) + '\n')
 
 if __name__ == '__main__':
     main()
