be2fce233c2ccc0eceb517f807b3f4ef36a32143
Steven Xu <stevenxxiu@gmail.com>
2015-09-20 19:04:29 +1000

    finally got the full data set from the unitn guys, huge thanks to them!

--- senti/data/twitter.py
+++ senti/data/twitter.py
@@ -13,21 +13,23 @@ def unique(seq):
 
 def main():
     os.chdir('data/twitter')
-    for name in ('dev', 'train'):
-        with open('input/duplicated/{}.json'.format(name)) as in_sr, open('{}.json'.format(name), 'w') as out_sr:
-            lines = unique(in_sr.read().splitlines())
-            for i, line in enumerate(lines):
-                text, label = next(iter(json.loads(line).items()))
-                out_sr.write(json.dumps({'id': '{}_{}'.format(name, i), 'text': text, 'label': int(label)//5}) + '\n')
+    class_map = {'negative': 0, 'neutral': 1, 'positive': 2}
+    for name, path in [
+        ('dev', 'input/unitn/dev/gold/twitter-dev-gold-B.tsv'),
+        ('train', 'input/unitn/train/cleansed/twitter-train-cleansed-B.txt')
+    ]:
+        with open(path) as sr, open('{}.json'.format(name), 'w') as out_sr:
+            for line in sr:
+                doc_id, label, text = re.match(r'\d+\t(\d+)\t(negative|neutral|positive)\t(.+)', line).groups()
+                text = text.encode().decode('unicode-escape')
+                out_sr.write(json.dumps({'id': doc_id, 'text': text, 'label': class_map[label]}) + '\n')
     with open('input/test/SemEval2015-task10-test-B-input.txt') as in_sr, \
             open('input/test/SemEval2015-task10-test-B-gold.txt') as labels_sr, open('test.json', 'w') as out_sr:
         for line, label_line in zip(in_sr, labels_sr):
-            doc_id, text = re.match(r'^NA\t(T\d+)\tunknwn\t(.+)', line).groups()
+            doc_id, text = re.match(r'NA\t(T\d+)\tunknwn\t(.+)', line).groups()
             doc_id_label, label = re.match(r'\d+\t(T\d+)\t(negative|neutral|positive)', label_line).groups()
             assert doc_id == doc_id_label
-            out_sr.write(json.dumps({
-                'id': doc_id, 'text': text, 'label': {'negative': 0, 'neutral': 1, 'positive': 2}[label]
-            }) + '\n')
+            out_sr.write(json.dumps({'id': doc_id, 'text': text, 'label': class_map[label]}) + '\n')
 
 if __name__ == '__main__':
     main()
--- senti/preprocess/preprocess.py
+++ senti/preprocess/preprocess.py
@@ -5,6 +5,7 @@ __all__ = ['normalize']
 
 
 def normalize(text):
+    text = text.replace('\u2019', '\'')
     text = re.sub(r'\S{,4}://\S+', '_url', text)
     text = re.sub(r'\$[\d\.]+', '_money', text)
     text = re.sub(r'(RT\s*)?@\w+:?', '_user', text)
