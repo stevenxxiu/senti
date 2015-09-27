6453e5364c327134c495f156845a2a18a34b96df
Steven Xu <stevenxxiu@gmail.com>
2015-09-27 21:07:17 +1000

    slightly better normalization

--- senti/preprocess/preprocess.py
+++ senti/preprocess/preprocess.py
@@ -14,5 +14,6 @@ def unescape(text):
 def normalize(text):
     text = re.sub(r'\S{,4}://\S+', ' _url ', text)
     text = re.sub(r'[@＠][a-zA-Z0-9_]+:?', ' _user ', text)
-    text = re.sub(r'([a-zA-Z.])\1+', r'\1\1', text)
+    text = re.sub(r'([a-zA-Z])\{2,}', r'\1\1', text)
+    text = re.sub(r'\.{4,}', r'...', text)
     return text.strip()
