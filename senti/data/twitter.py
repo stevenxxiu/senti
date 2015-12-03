cb3254abc930c45a234a85055564f52c1b56fe39
Steven Xu <stevenxxiu@gmail.com>
2015-12-03 11:58:32 +1100

    semeval & unsupervised & emoticon data fixes

--- senti/data/twitter.py
+++ senti/data/twitter.py
@@ -15,30 +15,7 @@ def unique(seq):
     return [x for x in seq if not (x in seen or seen_add(x))]
 
 
-def main():
-    os.chdir('data/twitter')
-    class_map = {'negative': 0, 'neutral': 1, 'positive': 2}
-    for name, path in [
-        ('dev', 'input/unitn/dev/gold/twitter-dev-gold-B.tsv'),
-        ('train', 'input/unitn/train/cleansed/twitter-train-cleansed-B.txt')
-    ]:
-        with open(path) as sr, open('{}.json'.format(name), 'w') as out_sr:
-            for line in sr:
-                doc_id, label, text = re.match(r'\d+\t(\d+)\t(negative|neutral|positive)\t(.+)', line).groups()
-                text = text.encode().decode('unicode-escape')
-                text = html.unescape(text)
-                out_sr.write(json.dumps({'id': doc_id, 'text': text, 'label': class_map[label]}) + '\n')
-    with open('input/test/SemEval2015-task10-test-B-input.txt') as in_sr, \
-            open('input/test/SemEval2015-task10-test-B-gold.txt') as labels_sr, open('test.json', 'w') as out_sr:
-        for line, label_line in zip(in_sr, labels_sr):
-            doc_id, text = re.match(r'NA\t(T\d+)\tunknwn\t(.+)', line).groups()
-            text = html.unescape(text)
-            doc_id_label, label = re.match(r'\d+\t(T\d+)\t(negative|neutral|positive)', label_line).groups()
-            assert doc_id == doc_id_label
-            out_sr.write(json.dumps({'id': doc_id, 'text': text, 'label': class_map[label]}) + '\n')
-    # with open('input/unsup/unsup.txt', encoding='utf-8') as in_sr, open('unsup.txt', 'w', encoding='utf-8') as out_sr:
-    #     for line in in_sr:
-    #         out_sr.write(html.unescape(line))
+class UnsupData:
     # full tokenizing is slow, so we use a less complicated re
     emoticon_re = re.compile(r'''
         (?:
@@ -55,27 +32,100 @@ def main():
             [<>]?
         )
     ''', re.VERBOSE)
-    # all emoticons as cache stage, as emoticons extraction can be very slow
-    # with open('unsup.txt', encoding='utf-8') as in_sr, open('emote.txt', 'w') as out_sr:
-    #     for i, line in enumerate(in_sr):
-    #         if emoticon_re_.search(line):
-    #             out_sr.write(line)
-    retweet_re = re.compile(r'RT\s*"?[@＠][a-zA-Z0-9_]+:?')
-    with open('emote.txt', encoding='utf-8') as in_sr, ExitStack() as stack:
-        out_srs = {i: stack.enter_context(open('emote_{}.txt'.format(i), 'w', encoding='utf-8')) for i in [0, 2]}
-        for i, line in enumerate(in_sr):
-            if retweet_re.search(line):
-                continue
-            counts = [0, 0, 0]
-            for match in emoticon_re.finditer(line):
-                counts[Emoticons.assess_match(match)] += 1
-            label = None
-            if counts[0] > 0 and counts[1] == 0 and counts[2] == 0:
-                label = 0
-            elif counts[0] == 0 and counts[1] == 0 and counts[2] > 0:
-                label = 2
-            if label is not None:
-                out_srs[label].write(emoticon_re.sub(' ', line))
+
+    @staticmethod
+    def unescape_unsup():
+        with open('input/unsup/unsup.txt', encoding='utf-8') as in_sr, \
+                open('unsup.txt', 'w', encoding='utf-8') as out_sr:
+            for line in in_sr:
+                out_sr.write(html.unescape(html.unescape(line)))
+
+    @classmethod
+    def write_all_emote(cls):
+        # all emoticons as cache stage, as emoticons extraction can be very slow
+        with open('unsup.txt', encoding='utf-8') as in_sr, open('emote.txt', 'w') as out_sr:
+            for i, line in enumerate(in_sr):
+                if cls.emoticon_re.search(line):
+                    out_sr.write(line)
+
+    @classmethod
+    def write_split_emote(cls):
+        retweet_re = re.compile(r'RT\s*"?[@＠][a-zA-Z0-9_]+:?')
+        with open('emote.txt', encoding='utf-8') as in_sr, ExitStack() as stack:
+            out_srs = {i: stack.enter_context(open('emote_{}.txt'.format(i), 'w', encoding='utf-8')) for i in [0, 2]}
+            for i, line in enumerate(in_sr):
+                if retweet_re.search(line):
+                    continue
+                counts = [0, 0, 0]
+                for match in cls.emoticon_re.finditer(line):
+                    counts[Emoticons.assess_match(match)] += 1
+                label = None
+                if counts[0] > 0 and counts[1] == 0 and counts[2] == 0:
+                    label = 0
+                elif counts[0] == 0 and counts[1] == 0 and counts[2] > 0:
+                    label = 2
+                if label is not None:
+                    out_srs[label].write(cls.emoticon_re.sub(' ', line))
+
+
+class SemEvalData:
+    class_map = {'negative': 0, 'neutral': 1, 'positive': 2}
+
+    @classmethod
+    def write_unitn(cls, out_path, unitn_path, download_path, is_train):
+        with open(unitn_path) as unitn_sr, open(download_path) as download_sr, open(out_path, 'w') as out_sr:
+            for unitn_line, download_line in zip(unitn_sr, download_sr):
+                doc_id_unitn, label_unitn, text_unitn = \
+                    re.match(r'\d+\t(\d+)\t(negative|neutral|positive)\t(.+)', unitn_line).groups()
+                doc_id_download, label_download, text_download = \
+                    re.match(r'\d+\t(\d+)\t(negative|neutral|positive)\t('r'.+)', download_line).groups()
+                text_unitn = text_unitn.encode().decode('unicode-escape')
+                text_unitn = text_unitn.replace(r'’', '\'')
+                if is_train:
+                    text_unitn = html.unescape(text_unitn)
+                    text_unitn = text_unitn.replace('""', '"')
+                text_download = html.unescape(html.unescape(text_download))
+                assert doc_id_unitn == doc_id_download
+                assert label_unitn == label_download
+                text = text_unitn
+                if text_download != 'Not Available':
+                    # some differences are impossible to reconcile, some unitn data have the wrong order
+                    # if re.sub(r'\s+', ' ', text_unitn) != re.sub(r'\s+', ' ', text_download):
+                    #     print(out_path)
+                    #     print(text_unitn)
+                    #     print(text_download)
+                    # assert re.sub(r'\s+', ' ', text_unitn) == re.sub(r'\s+', ' ', text_download)
+                    text = text_download
+                out_sr.write(json.dumps({'id': doc_id_unitn, 'text': text, 'label': cls.class_map[label_unitn]}) + '\n')
+
+    @classmethod
+    def write_test(cls, out_path, download_path, test_path):
+        with open(download_path) as in_sr, open(test_path) as labels_sr, open(out_path, 'w') as out_sr:
+            for line, label_line in zip(in_sr, labels_sr):
+                doc_id, text = re.match(r'NA\t(T\d+)\tunknwn\t(.+)', line).groups()
+                text = html.unescape(html.unescape(text))
+                doc_id_label, label = re.match(r'\d+\t(T\d+)\t(negative|neutral|positive)', label_line).groups()
+                assert doc_id == doc_id_label
+                out_sr.write(json.dumps({'id': doc_id, 'text': text, 'label': cls.class_map[label]}) + '\n')
+
+
+def main():
+    os.chdir('data/twitter')
+    # UnsupData.unescape_unsup()
+    # UnsupData.write_all_emote()
+    # UnsupData.write_split_emote()
+    for unitn_entry in [(
+        'dev.json', 'input/unitn/dev/gold/twitter-dev-gold-B.tsv',
+        'input/dev/gold/twitter-dev-gold-B-downloaded.tsv', False
+    ), (
+        'train.json', 'input/unitn/train/cleansed/twitter-train-cleansed-B.txt',
+        'input/train/cleansed/twitter-train-cleansed-B-downloaded.tsv', True
+    )]:
+        SemEvalData.write_unitn(*unitn_entry)
+    SemEvalData.write_test(
+        'test.json', 'input/test/SemEval2015-task10-test-B-input.txt',
+        'input/test/SemEval2015-task10-test-B-gold.txt'
+    )
 
 if __name__ == '__main__':
     main()
