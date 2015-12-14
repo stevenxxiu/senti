91d088629f4c44fa1b15c7acc13589f179e97321
Steven Xu <stevenxxiu@gmail.com>
2015-12-15 02:16:03 +1100

    still better to retrain all layers

--- senti/senti_models.py
+++ senti/senti_models.py
@@ -289,14 +289,14 @@ class SentiModels:
             ('normalize', MapTokens(normalize_special)),
             ('embeddings', embeddings_),
         ])
-        cf_dist = CachedFitTransform(CNNChar(
+        classifier = CachedFitTransform(CNNChar(
             batch_size=128, embeddings=embeddings_, input_size=140, output_size=3
         ), self.memory)
         kw = dict(dev_X=ft.transform(self.dev_docs), dev_y=self.dev_labels(), average_classes=[0, 2])
-        cf_dist.fit(ft.transform(distant_docs), distant_labels(), epoch_size=10**4, max_epochs=100, **kw)
-        cf = NNShallow(batch_size=128, model=cf_dist, num_train=1)
-        cf.fit(ft.transform(self.train_docs), self.train_labels(), max_epochs=10, **kw)
-        estimator = Pipeline([('features', ft), ('cf', cf)])
+        classifier.fit(ft.transform(distant_docs), distant_labels(), epoch_size=10**4, max_epochs=100, **kw)
+        # classifier = NNShallow(batch_size=128, model=classifier, num_train=5)
+        classifier.fit(ft.transform(self.train_docs), self.train_labels(), max_epochs=10, **kw)
+        estimator = Pipeline([('features', ft), ('classifier', classifier)])
         return 'cnn_char', estimator
 
     def fit_rnn(self):
