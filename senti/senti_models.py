cffb331a39a11391911807cd5f45b81bad817b4d
Steven Xu <stevenxxiu@gmail.com>
2015-12-16 00:52:04 +1100

    don't need to cache the twitter training phase

--- senti/senti_models.py
+++ senti/senti_models.py
@@ -295,14 +295,13 @@ class SentiModels:
             ('typos', IntroduceTypos(alphabet)),
             ('embeddings', embeddings_)
         ])
-        classifier = CachedFitTransform(CNNChar(
-            batch_size=128, embeddings=embeddings_, input_size=140, output_size=3
-        ), self.memory)
+        cf = CNNChar(batch_size=128, embeddings=embeddings_, input_size=140, output_size=3)
         kw = dict(dev_X=ft.transform(self.dev_docs), dev_y=self.dev_labels(), average_classes=[0, 2])
-        classifier.fit(ft.transform(distant_docs), distant_labels(), epoch_size=10**4, max_epochs=100, **kw)
-        # classifier = NNShallow(batch_size=128, model=classifier, num_train=5)
-        classifier.fit(ft_typo.transform(self.train_docs), self.train_labels(), max_epochs=10, **kw)
-        estimator = Pipeline([('features', ft), ('classifier', classifier)])
+        cf_dist = CachedFitTransform(cf, self.memory)
+        cf_dist.fit(ft.transform(distant_docs), distant_labels(), epoch_size=10**4, max_epochs=100, **kw)
+        # cf = NNShallow(batch_size=128, model=classifier, num_train=5)
+        cf.fit(ft_typo.transform(self.train_docs), self.train_labels(), max_epochs=10, **kw)
+        estimator = Pipeline([('features', ft), ('classifier', cf)])
         return 'cnn_char', estimator
 
     def fit_rnn_word(self):
