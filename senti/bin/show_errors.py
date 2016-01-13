0ce55fe6d4dbcce83216c1eb8937caeb47edb964
Steven Xu <stevenxxiu@gmail.com>
2016-01-14 04:39:23 +1100

    some refactoring of the error analysis

--- senti/bin/show_errors.py
+++ senti/bin/show_errors.py
@@ -9,7 +9,8 @@ def main():
     # shows errors sorted by worst probability
     errors = []
     os.chdir('data/twitter')
-    with open('results/test/cnn_word(embedding=google).json') as res_sr, open('semeval/test.json', 'r') as gold_sr:
+    name = 'cnn_word(embedding=google)'
+    with open('results/test/{}.json'.format(name)) as res_sr, open('semeval/test.json', 'r') as gold_sr:
         for res_line, gold_line in zip(res_sr, gold_sr):
             res_obj, gold_obj = json.loads(res_line), json.loads(gold_line)
             if res_obj['label'] != gold_obj['label']:
@@ -19,7 +20,7 @@ def main():
                     ('probs', res_obj['probs']),
                 ]))
     errors.sort(key=lambda obj_: dict(obj_['probs'])[obj_['gold_label']])
-    with open('results/test/cnn_word(embedding=google).errors.json', 'w') as sr:
+    with open('results/test/{}.errors.json'.format(name), 'w') as sr:
         for obj in errors:
             sr.write(json.dumps(obj) + '\n')
 
