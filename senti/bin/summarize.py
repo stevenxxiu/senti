016cb17fb98b640781eabfde6924f058190c0704
Steven Xu <stevenxxiu@gmail.com>
2016-01-31 06:59:01 +1100

    continue to work in the absence of test results

--- senti/bin/summarize.py
+++ senti/bin/summarize.py
@@ -7,9 +7,9 @@ import pandas as pd
 
 
 def main():
-    os.chdir('data/twitter')
+    os.chdir('data/twitter/semeval_2016_submit')
     names = []
-    for file_name in os.listdir('results/test'):
+    for file_name in os.listdir('results/val'):
         name, ext = file_name.split('.', 1)
         if ext == 'txt':
             names.append(name)
@@ -17,12 +17,16 @@ def main():
     for name in names:
         obj = OrderedDict([('name', name)])
         for test in ('val', 'test'):
-            with open('results/{}/{}.txt'.format(test, name)) as sr:
-                metrics = dict(zip(['accuracy', 'f1'], [float(line.split(':')[-1]) for line in sr.readlines()[-2:]]))
-                obj.update([('{}_{}'.format(test, name), value) for name, value in metrics.items()])
+            if os.path.exists('results/{}/{}.txt'.format(test, name)):
+                with open('results/{}/{}.txt'.format(test, name)) as sr:
+                    metrics = list(zip(['accuracy', 'f1'], [float(line.split(':')[-1]) for line in sr.readlines()[-2:]]))
+                    obj.update([('{}_{}'.format(test, name), value) for name, value in metrics])
         objs.append(obj)
     df = pd.DataFrame(objs, columns=objs[0].keys())
-    df.sort_values('test_f1', ascending=False, inplace=True)
+    try:
+        df.sort_values('test_f1', ascending=False, inplace=True)
+    except KeyError:
+        df.sort_values('val_f1', ascending=False, inplace=True)
     pd.options.display.max_colwidth = 1000
     print(df.to_string(index=False))
 
